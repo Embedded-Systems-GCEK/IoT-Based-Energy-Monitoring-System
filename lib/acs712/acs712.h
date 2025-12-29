2c648c0ff156387b5cc0b77fc8bc121e9f75cfab
Arun CS <arun.cs@etlab.in>
2025-12-29 13:38:08 -0500

    update

--- lib/acs712/acs712.h
+++ lib/acs712/acs712.h
@@ -1,11 +1,10 @@
 // Author  : FathimathSafaKT
 // Date    : 12-12-2025
 
-
 /*
 1.Initialize current sensor
-2.Read values from the current sensor 
-3.Convert sensor reading to current 
+2.Read values from the current sensor
+3.Convert sensor reading to current
 4.Calculate energy and power
 */
 // Function Prototype
@@ -18,6 +17,7 @@ int readCurrentSensor();
 float convertToCurrent(int adcValue);
 void calculatePowerAndEnergy(float current);
 
-extern float currentValue = 0.0;       // Calculated current
-extern float powerValue = 0.0;         // Power in watts
-extern float energyValue = 0.0;  
+extern float currentValue = 0.0; // Calculated current
+extern float powerValue = 0.0;   // Power in watts
+extern float energyValue = 0.0;
+#endif
