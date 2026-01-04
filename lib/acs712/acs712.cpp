d1b49c76297965b51f956e9ddeb53b1f353e2bbd
fathimathsafakt <fathimathsafakt86@gmail.com>
2026-01-04 15:23:50 +0400

    fix

--- lib/acs712/acs712.cpp
+++ lib/acs712/acs712.cpp
@@ -3,7 +3,8 @@
 
 // Global Variables
 int rawADC = 0;            // Raw ADC value
-float sensorVoltage = 0.0; // Voltage from ACS712 float currentValue = 0.0;       // Calculated current
+float sensorVoltage = 0.0; // Voltage from ACS712 
+float currentValue = 0.0;       // Calculated current
 float powerValue = 0.0;         // Power in watts
 float energyValue = 0.0;        // Energy in watt-hour (Wh)
 
